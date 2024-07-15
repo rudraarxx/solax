4e9f5104775bd87beb92815f4394cf12d059f24b
Rudra <rupesh@teamcodesign.com>
2024-07-15 23:52:05 +0530

    hero-section created

---
+++ script.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      primary: ["Jost", "sans-serif"],
+      secondary: ["Satoshi", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "992px",
+        xl: "1280px",
+        "2xl": "1440px",
+        "3xl": "1920px",
+      },
+      colors: {
+        primary: {
+          black: "#151515",
+          white: "#F5F5F5",
+          yellow: "#EEBB0C",
+        },
+      },
+    },
+  },
+};
